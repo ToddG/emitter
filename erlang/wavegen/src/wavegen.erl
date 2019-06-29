af083960099ed2a3cb369fef6634bc15d148a1fe
Todd Greenwood-Geer <toddg@zwrob.com>
2019-06-29 11:42:58 -0700

    add prometheus lib

--- erlang/wavegen/src/wavegen.erl
+++ erlang/wavegen/src/wavegen.erl
@@ -10,6 +10,8 @@
 %% escript Entry point
 main(Args) ->
     io:format("Args: ~p~n", [Args]),
+    prometheus:start(),
+    register_metrics(),
     [tick(500, F, 0) || F <- [fun flatline/1, fun flipflop/1, fun sinewave/1, fun incrementer/1]],
     %% this never get's called
     receive
@@ -22,9 +24,35 @@ main(Args) ->
 %%====================================================================
 %%
 %%--------------------------------------------------------------------
+%% register metrics with prometheus library
+%% this must be done prior to using metrics
+%%--------------------------------------------------------------------
+register_metrics() ->
+    prometheus_counter:new([{name, mycounter}, {help, "count the times each method has been invoked"}, {labels, [flatline, incrementer, flipflop, sinewave]}]),
+    prometheus_gauge:new([{name, myguage}, {help, "current value of a method"}, {labels, [flatline, incrementer, flipflop, sinewave]}]),
+    prometheus_summary:new([{name, mysummary}, {help, "summary of a method"}, {labels, [flatline, incrementer, flipflop, sinewave]}]),
+    prometheus_histogram:new([{name, myhist}, {help, "hist of a method"}, {labels, [method]},
+                              {buckets, [10, 100, 1000, 100000, 1000000]}]),
+    ok.
+
+%%--------------------------------------------------------------------
+%% multimetric : emit all the metrics since we're just trying to
+%% sort out how all this works. this way we can see stuff in the
+%% grafana dashboard
+%%--------------------------------------------------------------------
+multimetric(Label, Value) ->
+    prometheus_counter:inc(mycounter, [Label], Value),
+    prometheus_gauge:set(myguage, [Label], Value),
+    prometheus_summary:observe(mysummary, [Label], Value),
+    prometheus_histogram:observe(myhist, [Label], Value).
+
+%%--------------------------------------------------------------------
 %% emit : side-effect for the function F
 %%--------------------------------------------------------------------
-emit(Name, Value) -> io:format("emit: [~p]~p~n", [Name, Value]).
+emit(Name, Value) -> 
+    io:format("emit: [~p]~p~n", [Name, Value]),
+    multimetric(Name, Value).
+
 
 %%--------------------------------------------------------------------
 %% flat line : emits the value 1 for a flat line shape
